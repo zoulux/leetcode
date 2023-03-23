441af2a2694cb8b4f6ff1650463c1f4717dcce1a
Jake <zouluxian@datagrand.com>
2023-03-23 23:11:45 +0800

    feat: 队列中最大数

--- jzoffer/59/main.go
+++ jzoffer/59/main.go
@@ -17,40 +17,46 @@ func main() {
 }
 
 type MaxQueue struct {
-	values    []int
-	maxValues []int
+	values []int
+	dq     []int
 }
 
 func Constructor() MaxQueue {
-	return MaxQueue{values: []int{}}
+	return MaxQueue{values: []int{}, dq: []int{}}
 }
 
 func (this *MaxQueue) Max_value() int {
-	return this.maxValues[0]
+	if len(this.dq) == 0 {
+		return -1
+	}
+	return this.dq[0]
 }
 
 func (this *MaxQueue) Push_back(value int) {
 	this.values = append(this.values, value)
-	if len(this.maxValues) == 0 {
-		this.maxValues = append(this.maxValues, value)
-	} else {
-		_max := this.maxValues[len(this.maxValues)-1]
-		if value > _max {
-			this.maxValues = append(this.maxValues, value)
-		} else {
-			this.maxValues = append(this.maxValues, _max)
-		}
+
+	for len(this.dq) != 0 && this.dq[len(this.dq)-1] < value {
+		this.dq = this.dq[:len(this.dq)-1]
 	}
+	this.dq = append(this.dq, value)
+
 }
 
 func (this *MaxQueue) Pop_front() int {
-
 	if len(this.values) == 0 {
 		return -1
 	}
 	x := this.values[0]
 
 	this.values = this.values[1:]
+
+	if len(this.dq) != 0 && x == this.dq[0] {
+		this.dq = this.dq[1:]
+	}
+
+	// 3 5 4
+	// 3 => 5 => 5 4
+
 	return x
 }
 
